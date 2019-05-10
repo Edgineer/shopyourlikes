95ac0566dfc26b9311e27191a89135e91caa4816
mattsan15 <mattmattsan15@aol.com>
2019-05-10 01:15:31 -0700

    Added frontend basics

--- webapp/src/main/frontend/src/App.js
+++ webapp/src/main/frontend/src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
-import Home from "./home/Home";
-import Demo from "./demo/Demo";
+import Linklist from "./linklist/Linklist"
+import Login from "./login/Login"
+import logoColor from "./img/logoColor.svg"
+
+/* border-left: 5px solid red;
+  background-color: #f1f1f1;
+  padding: 10px 20px; */
 
 class App extends Component {
   render() {
     return (
-    <Router>
+      <Router>
       <div className="">
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/demo">Demo</Link></li>
-        </ul>
+          <Link to="/" ><img className="App-sylLogo" src={logoColor} /></Link>
+          <ul>
+            <li><Link to="/linktree">Link Tree Page</Link></li>
+          </ul>
+          
+        
 
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/demo" component={Demo}/>
+        <Route exact path="/" component={Login}/>
+        <Route exact path="/linktree" component={Linklist}/>
       </div>
       </Router>
     );
