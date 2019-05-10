364d39bda255d2ca899685578e21e542a7b9d7b5
Edgineer <elopez42@g.ucla.edu>
2019-05-09 21:38:17 -0700

    added link and user models and modified pom files to include JPA dependencies

---
+++ model/src/main/java/com/connexity/demo/model/Link.java
@@ -0,0 +1,23 @@
+package com.connexity.demo.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Link {
+
+    private @Id @GeneratedValue Long id;
+    private String username;
+    private String title;
+    private String url;
+
+    Link(String username, String title, String url){
+        this.username = username;
+        this.title = title;
+        this.url = url;
+    }
+}
