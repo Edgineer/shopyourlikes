706b3760b50025a934842f6a2b2ef361b36d1c94
mattsan15 <mattmattsan15@aol.com>
2019-05-13 18:06:02 -0700

    Few changes to linklist

--- webapp/src/main/frontend/src/linklist/Linklist.js
+++ webapp/src/main/frontend/src/linklist/Linklist.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import logo from './../logo.svg';
 import './../App.css';
 
+
 var obj = JSON.parse('{"firstName":"John", "lastName":"Doe"}');
 var listLinks = [{title: "Google", url: "http://www.google.com"}, {title: "Reddit", url: "http://www.reddit.com"}, {title: "Youtube", url: "http://www.youtube.com"}];
 
+const MESSAGE_URL = "/links";
+
 class Linklist extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        linklist: null,
+        error: null
+    };
+  }
+
+  async componentDidMount() {
+    this.fetchMessage();
+  }
+
+  async fetchMessage() {
+    try {
+        const response = await axios.get(MESSAGE_URL);
+        this.setState({linklist: response.data});
+    } catch (error) {
+        this.setState({error: "Error!"});
+    }
+  }
+
   render() {
 
     return (
