0f2dddf27fd7bbf4d6c137485ebd03036efb708a
joelwdav <joelwdavidson@gmail.com>
2019-05-13 18:04:44 -0700

    Added login page

--- webapp/src/main/frontend/src/App.js
+++ webapp/src/main/frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import Linklist from "./linklist/Linklist"
-import Login from "./login/Login"
+import Login from "./login/Login.js"
 import logoColor from "./img/logoColor.svg"
 
 /* border-left: 5px solid red;
@@ -17,12 +17,14 @@ class App extends Component {
           <Link to="/" ><img className="App-sylLogo" src={logoColor} /></Link>
           <ul>
             <li><Link to="/linktree">Link Tree Page</Link></li>
+            <li><Link to="/login">Login Page</Link></li>
           </ul>
           
         
 
         <Route exact path="/" component={Login}/>
         <Route exact path="/linktree" component={Linklist}/>
+        <Route exact path="/login" component={Login}/>
       </div>
       </Router>
     );
--- webapp/src/main/frontend/src/login/Login.js
+++ webapp/src/main/frontend/src/login/Login.js
@@ -1,15 +1,82 @@
 import React, { Component } from 'react';
 import logo from './../logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import logoColor from ".././img/logoColor.svg"
 import './../App.css';
+import './Login.css';
 
 class Login extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signIn: true,
+    }
+  }
 
-    return (
-      <div className="App">
-        
-      </div>
-    );
+  handleNewAccount() {
+    this.setState({
+      signIn: !this.state.signIn
+    });
+  }
+
+  render() {
+    if (this.state.signIn === true)
+    {
+      return (
+        <div className="Login-title">
+          <ul className="Login-box">
+            <img id="Login-logo" src={logoColor} alt="ShopYourLikes"/>
+            {/* <h2 id="Login-subtext">Login to continue</h2> */}
+            <hr width="87%" align="left"/>
+            <p>Login with instagram</p>
+            <hr width="87%" align="left" color="#a5a5b2"/>
+
+
+            <form>
+              <input type="text" placeholder="Username" ></input>
+              <br />
+              <input type="text" placeholder="Password"></input>
+            </form>
+
+            <br/>
+            <p id="Login-button"><Link to="/">Log In</Link></p>
+            <br/>
+          </ul>
+
+          <br/>
+          <button onClick={() => this.handleNewAccount()} id="New-account-link">Don't have an account?</button>
+        </div>
+      );
+    }
+    else {
+      return (
+        <div className="Login-title">
+          <h1>ShopYourLikes</h1> 
+          <h2 id="Login-subtext">Enter your user information</h2>
+          <ul className="Login-box">
+            <p>Login with instagram</p>
+            <hr width="87%" align="left"/>
+
+            <form>
+              <input type="text" placeholder="Email" ></input>
+              <br />
+              <input type="text" placeholder="Username" ></input>
+              <br />
+              <input type="text" placeholder="Password"></input>
+              <br />
+              <input type="test" placeholder="Repeat password"></input>
+            </form>
+
+            <br/>
+            <p id="Login-button"><Link to="/">Create Account</Link></p>
+            <br/>
+          </ul>
+
+          <br/>
+          <button onClick={() => this.handleNewAccount()} id="New-account-link">Have an account?</button>
+        </div>
+      );
+    }
   }
 }
 
