50434d62010ed8693088aa01d28f5a287b3923d1
Edgineer <elopez42@g.ucla.edu>
2019-05-13 23:49:10 -0700

    commented out files to isolate testing of link package

--- webapp/src/main/java/com/connexity/demo/packUser/User.java
+++ webapp/src/main/java/com/connexity/demo/packUser/User.java
@@ -1,29 +1,29 @@
-package com.connexity.demo.packUser;
-
-import java.time.LocalDate;
-import lombok.Data;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-@Data
-@Entity
-public class User {
-    private @Id @GeneratedValue Long id;
-    private String firstname;
-    private String lastname;
-    private String username;
-    private String hash;
-    private String email;
-    private LocalDate date_joined;
-
-    User (String firstname, String lastname, String username, String hash, String email){
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.username = username; //check it does not already exist
-        this.hash = hash;
-        this.email = email;
-        this.date_joined = LocalDate.now();
-    }
-}
+//package com.connexity.demo.packUser;
+//
+//import java.time.LocalDate;
+//import lombok.Data;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+//
+//@Data
+//@Entity
+//public class User {
+//    private @Id @GeneratedValue Long id;
+//    private String firstname;
+//    private String lastname;
+//    private String username;
+//    private String hash;
+//    private String email;
+//    private LocalDate date_joined;
+//
+//    User (String firstname, String lastname, String username, String hash, String email){
+//        this.firstname = firstname;
+//        this.lastname = lastname;
+//        this.username = username; //check it does not already exist
+//        this.hash = hash;
+//        this.email = email;
+//        this.date_joined = LocalDate.now();
+//    }
+//}
